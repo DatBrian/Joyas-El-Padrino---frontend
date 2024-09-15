36c89127a3d98360ad235ae89c61a895bed94656
DatBrian <vikard.2005@hotmail.com>
2024-09-14 21:32:40 -0500

    feat: :bug: Codigo comentado
    Se comentaron secciones de codigo

--- src/components/Sellers/SellersDetails/SellerDetails.tsx
+++ src/components/Sellers/SellersDetails/SellerDetails.tsx
@@ -1,14 +1,14 @@
-import { getSelelrDetails } from "@/api/sellers";
-import { useParams } from "react-router-dom";
+// import { getSelelrDetails } from "@/api/sellers";
+// import { useParams } from "react-router-dom";
 
 const SellerDetails = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
 
-    const seller = getSelelrDetails(id);
+    // const seller = getSelelrDetails(id);
 
   return (
     <div className="p-4 flex-grow overflow-auto bg-slate-400 rounded-sm text-center">
-      <h1 className="text-3xl font-bold mb-4">{seller.nombre}</h1>
+      {/* <h1 className="text-3xl font-bold mb-4">{seller.nombre}</h1>
       <p>
         <strong>id:</strong> {seller._id}
       </p>
@@ -27,7 +27,7 @@ const SellerDetails = () => {
       <p>
         <strong>Correo:</strong> {seller.correo}
       </p>
-      {/* Añade más detalles aquí */}
+      Añade más detalles aquí */}
     </div>
   );
 };
