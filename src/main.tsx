13f8b325f74fa5c5d0ab52a507ee087b825a9fe0
Brian Kaleth Melo <vikard.2005@hotmail.com>
2025-01-11 13:55:03 -0500

    refactor: :boom: Maintenance
    The page has been put in maintenance mode and no info will be displayed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/Router.tsx";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./routes/Router.tsx";
+import { Maintenance } from "./pages/Maintenance/Maintenance.tsx";
 
+//?  Página en mantenimiento, retirar el componente y descomentar el RouterProvider
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Maintenance/>
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
---
+++ src/pages/Maintenance/Maintenance.tsx
@@ -0,0 +1,9 @@
+
+export const Maintenance = () => {
+    return (
+        <div className="h-screen">
+            <h1 className="text-5xl text-red-400 text-center mt-52">Maintenance</h1>
+            <p className="text-center mt-4">We are currently working on this page. Please come back later.</p>	
+        </div>
+    );
+}
